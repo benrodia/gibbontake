9c872b526a025cec79e7bdaae5f446e671370e4a
Ben Rodia <ben@bstudiosonline.com>
2023-03-30 16:50:53 -0400

    receive updated comic pages

---
+++ comics/horsestuck/0005/index.php
@@ -0,0 +1,7 @@
+<?php 
+        $path = "../../../";
+        include($path.'head.php');
+        include($path.'reader.php'); 
+        echo reader($path,'Horsestuck',4);
+        include($path.'foot.php');
+    ?>
---
+++ comics/horsestuck/0028/index.php
@@ -0,0 +1,7 @@
+<?php 
+        $path = "../../../";
+        include($path.'head.php');
+        include($path.'reader.php'); 
+        echo reader($path,'Horsestuck',27);
+        include($path.'foot.php');
+    ?>
